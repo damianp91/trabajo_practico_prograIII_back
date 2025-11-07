a2823ef6e8589876358058d2f05528e39346b818
Damian <d4m14n36@gmail.com>
2025-11-07 02:29:34 -0300

    se levanta express y conexion a BBDD

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-console.log("Hello world from JavaScript project!");
+// NOTE: importaciones
 
-// Example function
-function greet(name) {
-  return `Hello, ${name}!`;
-}
+// Se levanta express
+import express from "express";
+const app = express();
 
+// Importacin de variables de entorno para puerto
+import environments from "./src/api/config/environment.js";
+const PORT = environments.port;
 
-console.log(greet("World"));
+// Conexiones de BBDD para enviar sentencias sql
+import connection from "./src/api/database/db.js";
+import cors from "cors";
+
+// WARN:  Middlewares
+// cors() basico para permitir las solicitudes
+// express.json() que transforma el JSON de las peticions POST a objects JS
+app.use(cors());
+app.use(express.json());
+
+// NOTE: endpoints
+
+app.get("/", (req, res) =>{
+  res.send("Pagina principal");
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
