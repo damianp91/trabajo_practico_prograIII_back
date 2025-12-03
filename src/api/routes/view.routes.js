fb3eac355ba08a04231394f8ea66a1300e8b5674
Damian <d4m14n36@gmail.com>
2025-12-03 02:35:24 -0300

    acceso en back

--- src/api/routes/view.routes.js
+++ src/api/routes/view.routes.js
@@ -8,40 +8,40 @@ import { requireLogin } from "../middlewares/middlewares.js";
 router.get("/", requireLogin, vistaProductos);
 
 router.get("/consultar", requireLogin,(req, res) => {
-    res.render("getById", {
-        title: "Consultar producto",
-        about: "Consultar producto por id"
-    });
+  res.render("getById", {
+    title: "Consultar producto",
+    about: "Consultar producto por id"
+  });
 })
 
 router.get("/modificar", requireLogin, (req, res) => {
-    res.render("update", {
-        title: "Modificar producto",
-        about: "Actualizar el producto"
-    });
+  res.render("update", {
+    title: "Modificar producto",
+    about: "Actualizar el producto"
+  });
 });
 
 router.get("/crear", requireLogin, (req, res) => {
-    res.render("create", {
-        title: "Crear",
-        about: "Crear producto"
-    });
+  res.render("create", {
+    title: "Crear",
+    about: "Crear producto"
+  });
 });
 
 router.get("/login", (req, res) => {
-    res.render("login", {
-        title: "Login"
-    });
+  res.render("login", {
+    title: "Login"
+  });
 })
 
 router.post("/login", loginAcceso);
 
 router.post("/logout", cerrarSesion);
-  
+
 router.get("/eliminar", (req, res) => {
-    res.render("delete", {
-        title: "Borrar"
-    });
+  res.render("delete", {
+    title: "Borrar"
+  });
 });
 
 
